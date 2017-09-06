bd306f8c97146dda8476f0d030cfcea1200893bb
Ljubo Nikolic <ljubonikolic@gmail.com>
2017-09-06 02:24:07 +0200

    commit

--- aws.tf
+++ aws.tf
@@ -9,19 +9,19 @@ resource "aws_key_pair" "ljubon-key-webapp" {
   public_key = "${file(var.public_key_path)}"
 }
 
-# test
-output "Public_Web-1" {
+
+output "public_web" {
 	value = "${aws_eip.web-1.public_ip}"
 }
-output "Public_NAT_GW" {
+output "public_nat" {
 	value = "${aws_eip.nat.public_ip}"
 }
-output "Private_Web-1" {
+output "private_web" {
 	value = "${aws_instance.web-1.private_ip}"
 }
-output "Private_DB-1" {
+output "private_db" {
 	value = "${aws_instance.db-1.private_ip}"
 }
-output "Private_NAT_GW" {
+output "private_nat" {
 	value = "${aws_instance.nat.private_ip}"
 }
