3130d814947f1ffd0cee26f65379411d90a43c1c
Ljubo Nikolic <ljubonikolic@gmail.com>
2017-09-11 23:14:32 +0200

    Suse OS for Web/DB

--- variables.tf
+++ variables.tf
@@ -19,7 +19,8 @@ variable "availability_zone" {
 variable "amis" {
 	description = "AMIs by region"
 	#default = "ami-c998b6b2" # RedHat 14.04 LTS N. Virginia
-	default = "ami-d74be5b8" # RedHat 14.04 LTS Frankfurt -> RHEL-7.4_HVM_GA-20170808-x86_64-2-Hourly2-GP2
+	#default = "ami-d74be5b8" # RedHat 14.04 LTS Frankfurt -> RHEL-7.4_HVM_GA-20170808-x86_64-2-Hourly2-GP2
+	default = "ami-c425e4ab" # SUSE 12 Frankfurt -> suse-sles-12-sp2-v20161214-hvm-ssd-x86_64
 }
 
 variable "amis-nat" {
