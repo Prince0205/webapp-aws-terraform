972dee974e3b436fa95f3d118c5e369e6a7ba8a2
Ljubo Nikolic <ljubonikolic@gmail.com>
2017-09-13 00:06:26 +0200

    test

--- script/install_nat.sh
+++ script/install_nat.sh
@@ -20,6 +20,7 @@ sudo ssh-keyscan -H 10.0.0.100, web >> ~/.ssh/known_hosts
 sudo ssh-keyscan -H 10.0.1.100, db >> ~/.ssh/known_hosts
 sudo ssh-keyscan -H 10.0.0.10, nat >> ~/.ssh/known_hosts
 
+
 echo "*****************EPEL RELEASE*****************"
 sudo yum -y install wget telnet
 wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
