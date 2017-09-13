3447550de14ede4744897a249aa87e1c58ce48ee
Ljubo Nikolic <ljubonikolic@gmail.com>
2017-09-13 21:08:59 +0200

    test github hook

--- script/install_db.sh
+++ script/install_db.sh
@@ -13,6 +13,10 @@ sudo echo "10.0.0.100	web-server	web" >> /etc/hosts
 sudo echo "10.0.1.100	db-server	db" >> /etc/hosts
 sudo echo "10.0.0.10	nat-server	nat" >> /etc/hosts
 
-sudo ssh-keyscan -H 10.0.0.100, web >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H 10.0.1.100, db >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H 10.0.0.10, nat >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H 10.0.0.100 >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H 10.0.1.100 >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H 10.0.0.10 >> ~/.ssh/known_hosts
+
+sudo ssh-keyscan -H web >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H db >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H nat >> ~/.ssh/known_hosts
--- script/install_nat.sh
+++ script/install_nat.sh
@@ -11,16 +11,19 @@ echo "*****************DONE CONFIGURATION NAT-SERVER*****************"
 
 sudo ls -lart /home/ec2-users/.ssh/
 sudo chmod 666 /etc/hosts
-#sudo chmod 600 ~/.ssh/jenkins
+sudo chmod 600 ~/.ssh/jenkins
 
 sudo echo "10.0.0.100 web-server web" >> /etc/hosts
 sudo echo "10.0.1.100 db-server db" >> /etc/hosts
 sudo echo "10.0.0.10 nat-server nat" >> /etc/hosts
 
-sudo ssh-keyscan -H 10.0.0.100, web >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H 10.0.1.100, db >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H 10.0.0.10, nat >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H 10.0.0.100 >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H 10.0.1.100 >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H 10.0.0.10 >> ~/.ssh/known_hosts
 
+sudo ssh-keyscan -H web >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H db >> ~/.ssh/known_hosts
+sudo ssh-keyscan -H nat >> ~/.ssh/known_hosts
 
 echo "*****************EPEL RELEASE*****************"
 sudo yum -y install wget telnet
