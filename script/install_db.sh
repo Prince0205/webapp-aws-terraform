cc91d0246cd6ab58c6928611aea1c827ce41c14b
Ljubo Nikolic <ljubonikolic@gmail.com>
2017-09-14 01:11:48 +0200

    test github hook

--- script/install_db.sh
+++ script/install_db.sh
@@ -12,6 +12,6 @@ sudo echo "10.0.0.100	web-server	web" >> /etc/hosts
 sudo echo "10.0.1.100	db-server	db" >> /etc/hosts
 sudo echo "10.0.0.10	nat-server	nat" >> /etc/hosts
 
-sudo ssh-keyscan -H web >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H db >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H nat >> ~/.ssh/known_hosts
+sudo ssh-keyscan web >> ~/.ssh/known_hosts
+sudo ssh-keyscan db >> ~/.ssh/known_hosts
+sudo ssh-keyscan nat >> ~/.ssh/known_hosts
--- script/install_nat.sh
+++ script/install_nat.sh
@@ -15,9 +15,9 @@ sudo echo "10.0.0.100 web-server web" >> /etc/hosts
 sudo echo "10.0.1.100 db-server db" >> /etc/hosts
 sudo echo "10.0.0.10 nat-server nat" >> /etc/hosts
 
-sudo ssh-keyscan -H web >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H db >> ~/.ssh/known_hosts
-sudo ssh-keyscan -H nat >> ~/.ssh/known_hosts
+sudo ssh-keyscan web >> ~/.ssh/known_hosts
+sudo ssh-keyscan db >> ~/.ssh/known_hosts
+sudo ssh-keyscan nat >> ~/.ssh/known_hosts
 
 echo "*****************EPEL RELEASE*****************"
 sudo yum -y install wget telnet
